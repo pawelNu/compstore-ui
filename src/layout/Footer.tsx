d9e4677623c5878c0b7414d91d29d3141f63a2d1
pawelNu <pnurzynski17@gmail.com>
2023-10-23 18:50:51 +0200

    fix: centered footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,12 +1,15 @@
-import "./../static/styles/Footer.css"
+import "./../static/styles/Footer.css";
 
 export const Footer = () => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <p className="card-text footer container">
-                    GitHub 2023 - Designed and implemented by @pawelnu & @grz55
-                </p>
+        <div className="container px-1">
+            <div className="card">
+                <div className="card-body">
+                    <p className="card-text footer">
+                        GitHub 2023 - Designed and implemented by @pawelnu &
+                        @grz55
+                    </p>
+                </div>
             </div>
         </div>
     );
