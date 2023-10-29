d8fa65e2a51ed9a1c4b4e8519f38155d97c7c6d2
pawelNu <pnurzynski17@gmail.com>
2023-10-29 17:05:11 +0100

    refactor: change products display from two column to one

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -5,15 +5,15 @@ import { AddToCartButton } from "../../layout/components/buttons/AddToCartButton
 export const Products = () => {
     return (
         <div className="container p-2 mb-2">
-            <div className="row">
+            {/* <div className="row"> */}
                 {productsPC.map((data) => (
-                    <div key={data.id} className="col-sm-6 mb-2">
+                    <div key={data.id} className="col-sm-8 mb-2">
                         <div className="card">
                             <a href={"pc/" + data.id}>
                                 <h5 className="card-header">{data.name}</h5>
                             </a>
                             <div className="row g-0">
-                                <div className="col-md-4">
+                                <div className="col-md-3">
                                     <a href={"pc/" + data.id}>
                                         <img
                                             src={data.productImage}
@@ -26,7 +26,7 @@ export const Products = () => {
                                         />
                                     </a>
                                 </div>
-                                <div className="col-md-5">
+                                <div className="col-md-6">
                                     <div className="card-body">
                                         {data.feature.map((feature, index) => (
                                             <p
@@ -51,7 +51,7 @@ export const Products = () => {
                         </div>
                     </div>
                 ))}
-            </div>
+            {/* </div> */}
         </div>
     );
 };
