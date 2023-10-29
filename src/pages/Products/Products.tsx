871ab920feb77111d81077dcaf8cec703f065d87
pawelNu <pnurzynski17@gmail.com>
2023-10-29 17:24:30 +0100

    style: changed color and removed underscore from cart header and increase price font size

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -5,53 +5,52 @@ import { AddToCartButton } from "../../layout/components/buttons/AddToCartButton
 export const Products = () => {
     return (
         <div className="container p-2 mb-2">
-            {/* <div className="row"> */}
-                {productsPC.map((data) => (
-                    <div key={data.id} className="col-sm-8 mb-2">
-                        <div className="card">
-                            <a href={"pc/" + data.id}>
-                                <h5 className="card-header">{data.name}</h5>
-                            </a>
-                            <div className="row g-0">
-                                <div className="col-md-3">
-                                    <a href={"pc/" + data.id}>
-                                        <img
-                                            src={data.productImage}
-                                            className="img-fluid rounded-start"
-                                            style={{
-                                                width: "480px",
-                                                height: "240px",
-                                            }}
-                                            alt="Product"
-                                        />
-                                    </a>
+            {productsPC.map((data) => (
+                <div key={data.id} className="col-sm-8 mb-2">
+                    <div className="card">
+                        <a className="header-link" href={"pc/" + data.id}>
+                            <h5 className="card-header">{data.name}</h5>
+                        </a>
+                        <div className="row g-0">
+                            <div className="col-md-3">
+                                <a href={"pc/" + data.id}>
+                                    <img
+                                        src={data.productImage}
+                                        className="img-fluid rounded-start"
+                                        style={{
+                                            width: "480px",
+                                            height: "240px",
+                                        }}
+                                        alt="Product"
+                                    />
+                                </a>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card-body">
+                                    {data.feature.map((feature, index) => (
+                                        <p
+                                            className="card-text"
+                                            key={index + 1}
+                                        >
+                                            <b>{feature.featureName}: </b>
+                                            {feature.value}
+                                        </p>
+                                    ))}
                                 </div>
-                                <div className="col-md-6">
+                            </div>
+                            <div className="col-md-3">
+                                <div className="price-tag">
                                     <div className="card-body">
-                                        {data.feature.map((feature, index) => (
-                                            <p
-                                                className="card-text"
-                                                key={index + 1}
-                                            >
-                                                <b>{feature.featureName}: </b>
-                                                {feature.value}
-                                            </p>
-                                        ))}
+                                        <b>Price: </b>
+                                        <div>$ {data.price}</div>
                                     </div>
-                                </div>
-                                <div className="col-md-3">
-                                    <div className="price-tag">
-                                        <div className="card-body">
-                                            <b>Price: </b>$ {data.price}
-                                        </div>
-                                        <AddToCartButton />
-                                    </div>
+                                    <AddToCartButton />
                                 </div>
                             </div>
                         </div>
                     </div>
-                ))}
-            {/* </div> */}
+                </div>
+            ))}
         </div>
     );
 };
