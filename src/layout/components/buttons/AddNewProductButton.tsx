96ff5bccb4cc427816c5fb4738649ba0e8bc702a
pawelNu <pnurzynski17@gmail.com>
2023-11-02 16:41:37 +0100

    feat (8): added button Add new product

---
+++ src/layout/components/buttons/AddNewProductButton.tsx
@@ -0,0 +1,7 @@
+export const AddNewProductButton = () => {
+    return(
+        <div className="me-2">
+            <a className="btn btn-outline-secondary" href="/" role="button">Add new product</a>
+        </div>
+    )
+}
