1eb4b174c081d80682fdbb2a88fb3f66c6ccef25
pawelNu <pnurzynski17@gmail.com>
2023-12-01 16:11:14 +0100

    refactor(59): moved labels to separate file

--- src/pages/Products/PC/NewPC.tsx
+++ src/pages/Products/PC/NewPC.tsx
@@ -1,62 +1,8 @@
 import { NewProduct } from "../../../types/NewProduct";
+import { labelNames } from "./NewPCLabels";
 
 export const NewPC: React.FC = () => {
-    const labels: NewProduct[] = [
-        {
-            labelName: "Processor Brand",
-            htmlFor: "processorBrand",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "Processor Name",
-            htmlFor: "processorName",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "Graphics Card Brand",
-            htmlFor: "graphicsCardBrand",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "Graphics Card Name",
-            htmlFor: "graphicsCardName",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "RAM GB Capacity",
-            htmlFor: "ramGBCapacity",
-            type: "number",
-            placeholder: "test",
-        },
-        {
-            labelName: "Drive GB Capacity",
-            htmlFor: "driveGBCapacity",
-            type: "number",
-            placeholder: "test",
-        },
-        {
-            labelName: "Drive Type",
-            htmlFor: "driveType",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "Operating System",
-            htmlFor: "operatingSystem",
-            type: "text",
-            placeholder: "test",
-        },
-        {
-            labelName: "Price",
-            htmlFor: "price",
-            type: "number",
-            placeholder: "test",
-        },
-    ];
+    const labels: NewProduct[] = labelNames;
 
     return (
         <>
---
+++ src/pages/Products/PC/NewPCLabels.ts
@@ -0,0 +1,58 @@
+import { NewProduct } from "../../../types/NewProduct";
+
+export const labelNames: NewProduct[] = [
+    {
+        labelName: "Processor Brand",
+        htmlFor: "processorBrand",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "Processor Name",
+        htmlFor: "processorName",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "Graphics Card Brand",
+        htmlFor: "graphicsCardBrand",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "Graphics Card Name",
+        htmlFor: "graphicsCardName",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "RAM GB Capacity",
+        htmlFor: "ramGBCapacity",
+        type: "number",
+        placeholder: "test",
+    },
+    {
+        labelName: "Drive GB Capacity",
+        htmlFor: "driveGBCapacity",
+        type: "number",
+        placeholder: "test",
+    },
+    {
+        labelName: "Drive Type",
+        htmlFor: "driveType",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "Operating System",
+        htmlFor: "operatingSystem",
+        type: "text",
+        placeholder: "test",
+    },
+    {
+        labelName: "Price",
+        htmlFor: "price",
+        type: "number",
+        placeholder: "test",
+    },
+];
