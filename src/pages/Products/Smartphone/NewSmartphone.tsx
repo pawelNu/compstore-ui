80620e38f8c7d106baeb0d6990e00e0fe2d0ec4d
pawelNu <pnurzynski17@gmail.com>
2023-12-03 10:14:34 +0100

    refactor(59): change form structure

--- src/pages/Products/Smartphone/NewSmartphone.tsx
+++ src/pages/Products/Smartphone/NewSmartphone.tsx
@@ -1,39 +1,49 @@
 export const NewSmartphone: React.FC = () => {
     return (
         <>
-            <div className="row mb-3">
-                <label
-                    htmlFor="processorBrand"
-                    className="col-sm-2 col-form-label"
-                >
-                    Smartphone
-                </label>
-                <div className="col-sm-10">
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="processorBrand"
-                        placeholder="test"
-                    />
+            <form>
+                <div className="row mb-3">
+                    <label
+                        htmlFor="processorBrand"
+                        className="col-sm-2 col-form-label"
+                    >
+                        Smartphone
+                    </label>
+                    <div className="col-sm-10">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="processorBrand"
+                            placeholder="test"
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div className="row mb-3">
-                <label
-                    htmlFor="processorName"
-                    className="col-sm-2 col-form-label"
-                >
-                    Price
-                </label>
-                <div className="col-sm-10">
-                    <input
-                        type="number"
-                        className="form-control"
-                        id="processorName"
-                        placeholder="test"
-                    />
+                <div className="row mb-3">
+                    <label
+                        htmlFor="processorName"
+                        className="col-sm-2 col-form-label"
+                    >
+                        Price
+                    </label>
+                    <div className="col-sm-10">
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="processorName"
+                            placeholder="test"
+                        />
+                    </div>
                 </div>
-            </div>
+                <div className="d-flex justify-content-center">
+                    <button type="submit" className="btn btn-outline-primary">
+                        Add product
+                    </button>
+                    <a href="/" className="btn btn-outline-danger mx-2">
+                        Cancel
+                    </a>
+                </div>
+            </form>
         </>
     );
 };
