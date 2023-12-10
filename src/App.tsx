4398554668ff4d8d3470f02ded3737627de9dfdc
pawelNu <93542936+pawelNu@users.noreply.github.com>
2023-12-10 16:51:14 +0100

    feat(59): add new pc form
    * feat(59): created form that adapts the fields to the selected product category
    
    * refactor(59): moved labels to separate file
    
    * feat(59): added axios
    
    * feat(59): added combo data from backend to dropdown lists
    
    * refactor(59): deleted unnecessary file and change code structure
    
    * refactor(59): change form structure
    
    * feat(59): connected fronted post with backend
    
    * refactor(59): fixed import
    
    * refactor(59): resolve conflicts and reformat project code

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { CategoryBar } from "./pages/MainPage/components/CategoryBar";
 import { Footer } from "./layout/Footer";
 import { ShoppingCartButton } from "./pages/MainPage/components/ShoppingCartButton";
 import { ShoppingCart } from "./pages/ShoppingCart/ShoppingCart";
-import { AddNewProductForm } from "./forms/Products/AddNewProductForm";
+import { AddNewProductForm } from "./pages/Products/AddNewProductForm";
 import { PCDetails } from "./pages/Products/PC/PCDetails";
 import { PCs } from "./pages/Products/PC/PCs";
 
---
+++ src/pages/Products/Laptop/NewLaptop.tsx
@@ -0,0 +1,43 @@
+export const NewLaptop: React.FC = () => {
+  return (
+    <>
+      <form>
+        <div className="row mb-3">
+          <label htmlFor="processorBrand" className="col-sm-2 col-form-label">
+            Laptop
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="processorBrand"
+              placeholder="test"
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label htmlFor="processorName" className="col-sm-2 col-form-label">
+            Price
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="number"
+              className="form-control"
+              id="processorName"
+              placeholder="test"
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-outline-primary">
+            Add product
+          </button>
+          <a href="/" className="btn btn-outline-danger mx-2">
+            Cancel
+          </a>
+        </div>
+      </form>
+    </>
+  );
+};
