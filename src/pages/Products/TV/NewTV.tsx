7ddca0dabf5ded1f347b9247b7ad83623e102f15
pawelNu <pnurzynski17@gmail.com>
2023-12-10 12:36:12 +0100

    refactor(59): resolve conflicts and reformat project code

--- src/pages/Products/TV/NewTV.tsx
+++ src/pages/Products/TV/NewTV.tsx
@@ -1,49 +1,43 @@
 export const NewTV: React.FC = () => {
-    return (
-        <>
-            <form>
-                <div className="row mb-3">
-                    <label
-                        htmlFor="processorBrand"
-                        className="col-sm-2 col-form-label"
-                    >
-                        TV
-                    </label>
-                    <div className="col-sm-10">
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="processorBrand"
-                            placeholder="test"
-                        />
-                    </div>
-                </div>
+  return (
+    <>
+      <form>
+        <div className="row mb-3">
+          <label htmlFor="processorBrand" className="col-sm-2 col-form-label">
+            TV
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="processorBrand"
+              placeholder="test"
+            />
+          </div>
+        </div>
 
-                <div className="row mb-3">
-                    <label
-                        htmlFor="processorName"
-                        className="col-sm-2 col-form-label"
-                    >
-                        Price
-                    </label>
-                    <div className="col-sm-10">
-                        <input
-                            type="number"
-                            className="form-control"
-                            id="processorName"
-                            placeholder="test"
-                        />
-                    </div>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <button type="submit" className="btn btn-outline-primary">
-                        Add product
-                    </button>
-                    <a href="/" className="btn btn-outline-danger mx-2">
-                        Cancel
-                    </a>
-                </div>
-            </form>
-        </>
-    );
+        <div className="row mb-3">
+          <label htmlFor="processorName" className="col-sm-2 col-form-label">
+            Price
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="number"
+              className="form-control"
+              id="processorName"
+              placeholder="test"
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-outline-primary">
+            Add product
+          </button>
+          <a href="/" className="btn btn-outline-danger mx-2">
+            Cancel
+          </a>
+        </div>
+      </form>
+    </>
+  );
 };
