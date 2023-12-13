5e05df315e43d1a17b56acf605996d356577a1e4
pawelNu <pnurzynski17@gmail.com>
2023-12-13 15:48:46 +0100

    feat(41): pc details connect with backend

--- src/data/PCDetailsData.ts
+++ src/data/PCDetailsData.ts
@@ -1,6 +1,6 @@
-import { PCDetails } from "../types/PCDetails";
+import { TPCDetails } from "../types/TPCDetails";
 
-export const pcDetails: PCDetails = {
+export const pcDetails: TPCDetails = {
   id: "83305d70-4a77-4811-a365-eeda3aa5f5fd",
   processorBrand: "Intel",
   processorName: "Intel Core i7 9gen 9700 2.9 - 4.0 GHz",
