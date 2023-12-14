cb6badd49b7edcaeec83b20f54a1bada5aa8372e
pawelNu <pnurzynski17@gmail.com>
2023-12-14 14:50:22 +0100

    feat(53): added filter button

---
+++ src/layout/components/buttons/FilterButton.tsx
@@ -0,0 +1,22 @@
+export const FilterButton = () => {
+    return(
+        <>
+      <a href="/" className="btn btn-primary">
+        Filter
+        <svg
+          className="ms-2"
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        >
+          <path
+            d="M19.479 2l-7.479 12.543v5.924l-1-.6v-5.324l-7.479-12.543h15.958zm3.521-2h-23l9 15.094v5.906l5 3v-8.906l9-15.094z"
+            fill="white"
+          />
+        </svg>
+      </a>
+    </>
+    )
+}
