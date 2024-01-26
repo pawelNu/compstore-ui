5fb3c41945f7c1ee899e530855f6821a140300b7
pawelNu <pnurzynski17@gmail.com>
2024-01-26 09:32:21 +0100

    feat(68): added setting connection frontend and backend on render

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,10 @@
 const environmentUrls = new Map();
 
 environmentUrls.set("localhost", "http://localhost:8080");
+environmentUrls.set(
+    "compstore-ui.onrender.com",
+    "https://compstore-api.onrender.com"
+);
 
 const hostName = environmentUrls.get(window.location.hostname);
 
