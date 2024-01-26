e792b002dbded775bc027feef862e4fb5377a80a
pawelNu <pnurzynski17@gmail.com>
2024-01-26 09:35:09 +0100

    style(68): reformat code

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ const environmentUrls = new Map();
 environmentUrls.set("localhost", "http://localhost:8080");
 environmentUrls.set(
     "compstore-ui.onrender.com",
-    "https://compstore-api.onrender.com"
+    "https://compstore-api.onrender.com",
 );
 
 const hostName = environmentUrls.get(window.location.hostname);
