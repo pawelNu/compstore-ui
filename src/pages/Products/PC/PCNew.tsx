c93aa699a672586300e17eb59bddaad125782d21
pawelNu <93542936+pawelNu@users.noreply.github.com>
2024-01-30 18:34:33 +0100

    refactor(61): removed unnecessary comment

--- src/pages/Products/PC/PCNew.tsx
+++ src/pages/Products/PC/PCNew.tsx
@@ -72,7 +72,6 @@ export const PCNew: React.FC = () => {
 
     return (
         <>
-            {/* TODO update form to formik form */}
             <form onSubmit={(e) => onSubmit(e)}>
                 <div className="row mb-3">
                     <label
